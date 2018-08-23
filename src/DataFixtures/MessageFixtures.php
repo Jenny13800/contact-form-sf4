75def17e13aa08cdc8ddf10edde339ad3f67f6ab
Jennifer <jcour@montmirail.com>
2018-08-23 14:38:10 +0200

    add admin in fixtures use UserPasswordEncoderInterface

--- src/DataFixtures/MessageFixtures.php
+++ src/DataFixtures/MessageFixtures.php
@@ -2,12 +2,23 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Admin;
 use App\Entity\Message;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class MessageFixtures extends Fixture
 {
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = \Faker\Factory::create('fr_FR');
@@ -23,6 +34,16 @@ class MessageFixtures extends Fixture
 
             $manager->persist($message);
         }
+
+        $admin = new Admin();
+
+        // demande à encoder un password
+        $password = $this->encoder->encodePassword($admin, "admin");
+
+        $admin->setUsername("admin")
+            ->setPassword($password);
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
