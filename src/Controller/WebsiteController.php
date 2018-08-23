898e65ec1d59c477dd66577c423b818064227079
Jennifer <jcour@montmirail.com>
2018-08-23 11:01:26 +0200

    configuration contact & boostratp form config twig Add function contact => createForm Create form Modify views

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\ContactType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WebsiteController extends AbstractController
@@ -10,7 +14,7 @@ class WebsiteController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index()
+    public function indexAction()
     {
         return $this->render('website/index.html.twig', [
             'controller_name' => 'WebsiteController',
@@ -20,7 +24,29 @@ class WebsiteController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(){
-        return $this->render('website/contact.html.twig');
+    public function contactAction(Request $request, ObjectManager $manager){
+
+        $message = new Message();
+
+        $form = $this->createForm(ContactType::class, $message);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $message->setIsTreated(false)
+                    ->setCreatedAt(new \DateTime());
+
+            $manager->persist($message);
+            $manager->flush();
+
+            return $this->redirectToRoute('accueil');
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('website/contact.html.twig', [
+                'form' => $formView
+            ]);
     }
 }
